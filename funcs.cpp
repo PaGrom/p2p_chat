886358eeeb0a3d1206183c3b7fab46b13191f183
PaGrom <pagrom23@gmail.com>
2013-03-09 15:24:03 +0400

    Converted spaces to tabs in funcs.cpp

--- funcs.cpp
+++ funcs.cpp
@@ -2,15 +2,15 @@
 
 string get_time() {
 	time_t timer;
-    char buffer[25];
-    struct tm* tm_info;
+	char buffer[25];
+	struct tm* tm_info;
 
-    time(&timer);
-    tm_info = localtime(&timer);
+	time(&timer);
+	tm_info = localtime(&timer);
 
-    strftime(buffer, 25, "%Y:%m:%d %H:%M:%S", tm_info);
+	strftime(buffer, 25, "%Y:%m:%d %H:%M:%S", tm_info);
 
-    return buffer;
+	return buffer;
 }
 
 void write_to_log(string logfile_name, string line) {
